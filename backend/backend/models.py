ff613ad689daa786af6a502083500ae6fbe69338
votuan3719 <votuan3719@gmail.com>
2025-10-30 11:41:31 -0500

    initial model
    upload models

---
+++ backend/backend/models.py
@@ -0,0 +1,48 @@
+## admin, staff, customer, menu, order
+from django.db import models
+from django.contrib.auth.models import User
+
+class Staff(models.Model):
+    staff_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
+
+class Menu(models.Model):
+    menu_id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    
+
+
+
